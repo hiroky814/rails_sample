46c1b73242eab3eff97b48d6eb97b0d2859da759
hiroky814 <hiroky.814@gmail.com>
2018-05-19 10:28:58 +0900

    add codeclimate

--- Gemfile
+++ Gemfile
@@ -35,6 +35,9 @@ gem 'jbuilder', '~> 2.5'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
+  gem 'rspec'
+  gem 'codeclimate-test-reporter'
+  gem 'simplecov'
 end
 
 group :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,8 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -93,4 +95,13 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+
+  # save to CircleCI's artifacts directory if we're on CircleCI
+  if ENV['CIRCLE_ARTIFACTS']
+    dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
+    SimpleCov.coverage_dir(dir)
+  end
+
+  SimpleCov.start
 end
