56e41c24639bafd1feadc229e348eb58e8011992
hiroki yoshida <yoshida@creationline.com>
2018-08-24 15:50:55 +0900

    変更

--- app/controllers/api/v1/pdf_url_controller.rb
+++ app/controllers/api/v1/pdf_url_controller.rb
@@ -1,7 +1,7 @@
 module Api::V1
 	class PdfUrlController < ActionController::API
     def read
-      render json: { url: 'https://s3-ap-northeast-1.amazonaws.com/pdf-test-hiroky/sample.pdf'}.to_json
+      render json: { url: ''}.to_json
     end
 	end
 end
